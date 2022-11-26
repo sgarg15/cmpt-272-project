0572f1ab29e76228227cdcab1972e4bbd322d936
Satvik Garg <47345135+sgarg15@users.noreply.github.com>
2022-11-25 22:45:13 -0800

    Added pig map

--- final-project/src/app/app.module.ts
+++ final-project/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PigReportListComponent } from './pig-report-list/pig-report-list.component';
 import { PigReportComponent } from './pig-report/pig-report.component';
+import { PigMapComponent } from './pig-map/pig-map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PigReportListComponent,
-    PigReportComponent
+    PigReportComponent,
+    PigMapComponent
   ],
   imports: [
     BrowserModule,
