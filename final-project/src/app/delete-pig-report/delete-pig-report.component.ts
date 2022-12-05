9ce7c712927222f9bacb93b8b45ebb9e8f66fc7a
Satvik Garg <47345135+sgarg15@users.noreply.github.com>
2022-12-04 16:23:51 -0800

    Added hashify to delete pig report

--- final-project/src/app/delete-pig-report/delete-pig-report.component.ts
+++ final-project/src/app/delete-pig-report/delete-pig-report.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PigReportInterface } from '../util/pigReport.module';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CrudService } from '../crud.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-delete-pig-report',
@@ -21,29 +22,31 @@ export class DeletePigReportComponent implements OnInit {
   constructor(
     private ActivatedRoute: ActivatedRoute,
     private crud: CrudService,
-    private router: Router
+    private router: Router,
+    private http: HttpClient
   ) {}
 
   togglePasswordVisibility() {
     this.passwordField = !this.passwordField;
   }
 
-  checkPassword() {
-    
-    if (this.password === 'OINK!!') {
-      this.passwordCheck = !this.passwordCheck;
-    } else {
-      if (!this.error) {
-        this.error = !this.error;
-      }
-    }
+  verifyPassword() {
+    this.http
+      .get('https://api.hashify.net/hash/md5/hex?value=' + this.password)
+      .subscribe((data: any) => {
+        if (data.Digest === '84892b91ef3bf9d216bbc6e88d74a77c') {
+          this.passwordCheck = !this.passwordCheck;
+        } else {
+          if (!this.error) {
+            this.error = !this.error;
+          }
+        }
+      });
   }
 
   deletePigReport() {
     this.passwordCheck = false;
     this.pigReport = this.crud.getPigReportByDate(this.givenDate);
-
-    
 
     if (this.pigReport) {
       this.crud.deletePigReport(this.pigReport);
